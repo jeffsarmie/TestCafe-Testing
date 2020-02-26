3d4080ea695ef041b8203d0fab1e630dff969788
Jeffsarmie <jeff.sarmiento@highspot.com>
2020-02-26 12:45:54 -0800

    adding test hooks

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -2,6 +2,21 @@ import { Selector } from 'testcafe'
 
 fixture `Getting Started with TestCafe`
     .page`https://devexpress.github.io/testcafe/example`
+    .before(aync t => { 
+        //Test goes here
+        //await runDatabaseReset()
+        //await seedTestData()
+    })
+    .beforeEach(async t => {
+        //Runs before each test
+    })
+    .after(async t => {
+        //Cleaning test data
+        //Logging and sending data to monitoring systems
+    })
+    .afterEach(async t => {
+        //Runs after each test
+    })
 
 test('My first testcafe test', async t => {
     await t.typeText('#developer-name', 'Jeff')
