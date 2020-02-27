d18db78e0d13c9114e11c61990435448055c3251
Jeffsarmie <jeff.sarmiento@highspot.com>
2020-02-27 13:36:06 -0800

    adding some apis

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -20,6 +20,7 @@ fixture `Getting Started with TestCafe`
         //Runs after each test
     })
 
+
 test('My first testcafe test', async t => {
     const dev_name_input = Selector('#developer-name')
     const submit_button = Selector('#submit-button')
@@ -29,4 +30,28 @@ test('My first testcafe test', async t => {
     //await t.wait(3000)
     await t.click(submit_button)
     await t.expect(articleText).contains('Jeff')
+
+    // Click
+    await t.click('selector', { options })
+    // Double Click
+    await t.doubleClick('selector', { options })
+    // Right Click
+    await t.rightClick('selector', { options })
+    // Drag
+    await t.drag('selector', 200, 0, { options })
+    // Hover
+    await t.hover('selector', { options })
+    // Select text
+    await t.selectText('selector')
+    // Type text
+    await t.typeText('selector')
+    // Press key on the keyboard
+    await t.pressKey('key')
+    // Navigate to URL
+    await t.navigateTo('url')
+    // Take Screenshot
+    await t.takeScreenshot()
+    await t.takeElementScreenshot()
+    // Resize Window
+    await t.resizeWindow(800, 600)
 })
