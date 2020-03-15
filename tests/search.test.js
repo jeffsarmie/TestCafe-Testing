c5c846a6bf48d4a4e75a0c28b5632373868ba469
Jeffsarmie <jeff.sarmiento@highspot.com>
2020-03-15 14:06:09 -0700

    adding page

--- tests/search.test.js
+++ tests/search.test.js
@@ -1,29 +1,25 @@
 import { Selector } from 'testcafe'
 import Navbar from '../page-objects/components/Navbar'
+import SearchResultsPage from '../page-objects/pages/searchresultspage'
 //import xPathToCss from 'xpath-to-css'
 
 const navbar = new Navbar()
+const searchResultsPage = new SearchResultsPage()
 
 fixture `Search Box Test`
     .page`http://zero.webappsecurity.com/index.html`
 
 test("User can search information via Search box", async t =>{
-    //Selectors
-    const title = Selector ('h2')
-    const linkText = Selector ('div').innerText
-
     //XPATH to CSS Examples
     // const xpath = `/html/body/div[1]/div[2]/div/div/div/div/h3`
     // const css = xPathToCss(xpath)
     // console.log(css)
-    
-    //Actions
     navbar.search('bank')
-
-    //Assertions
-    await t.expect(title.exists).ok()
+    await t.expect(searchResultsPage.resultsTitle.exists).ok()
     await t.expect(navbar.searchBox.exists).ok()
-    await t.expect(linkText).contains('Zero - Personal Banking')
+    await t
+        .expect(searchResultsPage.linkText.innerText)
+        .contains('Zero - Personal Banking')
 })
 
     
